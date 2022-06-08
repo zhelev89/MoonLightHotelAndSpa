5225dbaa5e0d60735034cdb78705985d336487f1
zhelev89 <zhelev89@yahoo.com>
2022-06-08 11:48:53 +0300

    create spring project and made commit to gitHub

---
+++ MoonLight-Hotel-and-Spa/src/main/java/team2/MoonLight/Hotel/and/Spa/MoonLightHotelAndSpaApplication.java
@@ -0,0 +1,13 @@
+package team2.MoonLight.Hotel.and.Spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonLightHotelAndSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonLightHotelAndSpaApplication.class, args);
+	}
+
+}
---
+++ MoonLight-Hotel-and-Spa/src/test/java/team2/MoonLight/Hotel/and/Spa/MoonLightHotelAndSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package team2.MoonLight.Hotel.and.Spa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonLightHotelAndSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
