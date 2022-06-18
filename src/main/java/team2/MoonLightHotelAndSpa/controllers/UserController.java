5d738187d4e6e5dc3dc63ee55c805cc2c2556667
zhelev89 <zhelev89@yahoo.com>
2022-06-18 23:22:19 +0300

    create user deleteBYID EndPoint, and put status code on exceptions

--- src/main/java/team2/MoonLightHotelAndSpa/controllers/UserController.java
+++ src/main/java/team2/MoonLightHotelAndSpa/controllers/UserController.java
@@ -54,7 +54,7 @@ public class UserController {
     public ResponseEntity<UserResponse> findById(@PathVariable @Valid Long id) {
         User foundUser = userService.findById(id);
         UserResponse userResponse = userConverter.convert(foundUser);
-        return ResponseEntity.status(HttpStatus.FOUND).body(userResponse);
+        return ResponseEntity.ok().body(userResponse);
     }
 
     @GetMapping
@@ -65,11 +65,17 @@ public class UserController {
                         .collect(Collectors.toList()));
     }
 
-    @PutMapping(value = "/update")
+    @PutMapping
     public ResponseEntity<UserResponse> update(@RequestBody @Valid UserUpdateRequest userUpdateRequest) {
         User user = userConverter.convert(userUpdateRequest);
         User updatedUser = userService.update(user.getId(), user.getPassword());
         UserResponse userResponse = userConverter.convert(updatedUser);
         return ResponseEntity.ok().body(userResponse);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable Long id) {
+        userService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
