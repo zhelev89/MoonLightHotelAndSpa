90644efbe93aaba637d8f1bf45437a02966dc46c
zhelev89 <zhelev89@yahoo.com>
2022-06-29 10:20:43 +0300

    put role UpperCase

--- src/main/java/team2/MoonLightHotelAndSpa/convertors/RoleConverter.java
+++ src/main/java/team2/MoonLightHotelAndSpa/convertors/RoleConverter.java
@@ -10,8 +10,8 @@ public class RoleConverter {
     private static final String role_prefix = "ROLE_";
 
     public String convertRoleRequest(String role) {
-        if (!role.startsWith(role_prefix)) {
-            return "ROLE_" + role.toUpperCase(Locale.ROOT);
+        if (!role.toUpperCase().startsWith(role_prefix)) {
+            return role_prefix + role.toUpperCase(Locale.ROOT);
         }
         return role.toUpperCase();
     }
