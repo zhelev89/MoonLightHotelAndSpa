7821f531046ca7fe09057d10308980c8327b8d00
Eglss <martinrmilev@gmail.com>
2022-07-25 17:58:45 +0300

    some fixes

---
+++ src/main/java/team2/MoonLightHotelAndSpa/dataTransferObject/roomReservation/RoomReservationQuery.java
@@ -0,0 +1,12 @@
+package team2.MoonLightHotelAndSpa.dataTransferObject.roomReservation;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RoomReservationQuery {
+    private String start_date;
+    private String end_date;
+    private int people;
+}
