e078875b7d78e3a3d3039f900ad452f28dec7633
vanol <iwolkkov@gmail.com>
2022-10-13 21:09:42 +0300

    configure web security

--- src/main/java/team2/MoonLightHotelAndSpa/security/SecurityConfig.java
+++ src/main/java/team2/MoonLightHotelAndSpa/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,6 +22,7 @@ import static team2.MoonLightHotelAndSpa.security.SecurityUrlConstant.*;
 @AllArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
@@ -42,10 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .mvcMatchers(HttpMethod.POST, PUBLIC_URL_POST).permitAll()
                 .mvcMatchers(HttpMethod.GET, PUBLIC_URL_GET).permitAll()
                 .mvcMatchers(HttpMethod.PUT, PUBLIC_URL_PUT).permitAll()
@@ -65,8 +64,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling()
                 .and()
-                .formLogin().disable()
-                .logout().disable()
-                .csrf().disable();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().cors();
     }
 }
