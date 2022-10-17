47fabfe9c4c66d8522bc019acd267b635be3eba2
YYovchev123 <102542112+YYovchev123@users.noreply.github.com>
2022-10-17 21:34:12 +0300

    security

--- src/main/java/team2/MoonLightHotelAndSpa/security/SecurityUrlConstant.java
+++ src/main/java/team2/MoonLightHotelAndSpa/security/SecurityUrlConstant.java
@@ -34,7 +34,8 @@ public class SecurityUrlConstant {
             "/users/reservations", "/users/{uid}/reservations", "/users/{uid}/reservations/{rid}",
             "/users/screens/reservations", "/users/{uid}/screens/reservations", "/users/{uid}/screens/reservation/{rid}",
             "/screens/{id}",
-            "/screens/{id}/reservations", "/screens/{id}/reservations/{rid}", "/users/transfers"
+            "/screens/{id}/reservations", "/screens/{id}/reservations/{rid}", "/users/transfers", "/users/{uid}/transfers/{tid}",
+            "/users/{uid}/transfers"
     };
     protected static final String[] PROTECTED_URL_GET_CLIENT = {
             "/users/profile",
